b27ecb5700e7be201bd88fa5f41a4fd2f4b3d2be
palme1337 <chris.palmer1337@icloud.com>
2015-10-08 00:52:14 -0700

    Pushing stuff before I break it

---
+++ src/cljs/alpaca_traders/mon-group.cljs
@@ -0,0 +1,46 @@
+(ns alpaca-traders.money-group)
+
+(def placeholders {
+                    :platinum "Platinum"
+                     :gold "Gold"
+                     :silver "Silver"
+                     :copper "Copper"
+                    })
+(def currency-to-copper {
+                          :platinum 10000
+                          :gold 1000
+                          :silver 100
+                          :copper 1
+                          })
+
+(defn rebalance [unbalanced-group]
+  (-> (:price unbalanced-group) to-coppers to-money-group)
+  )
+
+(defn money-amount-to-coppz [money-entry]
+  (let [currency (first money-entry)
+        amount (second money-entry)]
+    (* (currency currency-to-copper) amount))
+  )
+
+(defn to-coppers [money-group]
+  (reduce + (map money-amount-to-coppz (seq money-group)))
+  )
+
+
+(defn reduce-to-money-group [m]
+  (let [keys-left (:keys-left m)
+        current-key (first keys-left)
+        copper-left (:amount-left m)
+        conversion-rate (get currency-to-copper current-key)]
+    (if (= current-key nil)
+      (dissoc m :keys-left :amount-left)
+      (reduce-to-money-group
+        (assoc m current-key (int (/ copper-left conversion-rate))
+                 :keys-left (rest keys-left)
+                 :amount-left (rem copper-left conversion-rate))))))
+
+
+(defn to-money-group [copper-amount]
+  (let [k-order [:platinum :gold :silver :copper]]
+    (reduce-to-money-group {:amount-left copper-amount, :keys-left k-order})))
--- src/cljs/alpaca_traders/new-posting.cljs
+++ src/cljs/alpaca_traders/new-posting.cljs
@@ -1,10 +1,56 @@
 (ns alpaca-traders.new-posting
   (:require [reagent.core :as reagent :refer [atom]]
-             [alpaca-traders.money-group :as money-group]))
+   [alpaca-traders.money-group :as money-group :refer [to-coppers to-money-group rebalance placeholders]]))
+
+(def input-state (reagent/atom {
+                                 :price {
+                                          :platinum 0
+                                          :gold 0
+                                          :silver 0
+                                          :copper 0
+                                          }
+                                 :price-per-unit {
+                                                   :platinum 0
+                                                   :gold 0
+                                                   :silver 0
+                                                   :copper 0
+                                                   }
+                                 :quantity 1
+                                 }))
 
 
+(defn currency-input [param]
+  [:div [:label (:param placeholders)]
+   [:input {
+             :type "number"
+             :min "0"
+             :placeholder (:param placeholders)
+             :on-change #(swap! input-state assoc-in [:price param] (int (.-target.value %)))
+             :on-blur #(swap! input-state assoc :price (rebalance @input-state))
+             :value (get-in @input-state [:price param])
+             }]
+   ])
+
+(defn input-group [input-group-type]
+  [:div [currency-input :platinum input-group-type]
+  [currency-input :gold "Gold"]
+  [currency-input :silver "Silver"]
+  [currency-input :copper "Copper"]
+  ])
 
 (defn create []
   [:div
-   [money-group/create]
-    ])
+   [:h2 "State your price, dingus."]
+   [:label "Quantity"]
+   [:input {
+             :type "number"
+             :min "1"
+             :on-change #(swap! input-state assoc :quantity (int (.-target.value %)))
+             :value (:quantity @input-state)}]
+
+   [input-group :price]
+
+   [:h2 "Price Per Unit"]
+   [input-group :price-per-unit]
+   [:div (str @input-state)]
+   ])
