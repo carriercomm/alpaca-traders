4847019dba699ba6f9f8e361ac2b5907e3c60519
palme1337 <chris.palmer1337@icloud.com>
2015-11-01 22:26:35 -0800

    Playing with re-frame and routing
    Ajax request in new post
    
    And a little bit more CSS clean up.
    
    stash

---
+++ src/cljs/alpaca_traders/db.cljs
@@ -0,0 +1,34 @@
+(ns alpaca-traders.db
+  (:require 
+    [alpaca-traders.money-group :as money])
+  )
+
+(def test-items [{:name "Choose an item" :id nil}
+                 {:name "Bone chips" :id 1}
+                 {:name "Duck chips" :id 2}
+                 ]
+  )
+
+(def test-servers [{:name "Choose a server" :id nil}
+                   {:name "Mal'Ganis" :id 1}
+                   {:name "Tyrael" :id 2}
+                   ]
+  )
+
+(def test-listings [{:contact-name "Nordic Sean" 
+                     :price money/default-group
+                     :quantity 2000}]
+  )
+
+(defn get-listings [item server] 
+  test-listings
+  )
+
+
+(def initial-state 
+  {:items test-items 
+   :listings (sorted-map) ;;todo sort by field
+   :selected-item nil 
+   :servers test-servers
+   :selected-server nil}
+  )
---
+++ src/cljs/alpaca_traders/search.cljs
@@ -0,0 +1,49 @@
+(ns alpaca-traders.search
+  (:require [reagent.core :as r :refer [atom]]
+            [re-frame.core :refer [subscribe dispatch]]
+            [alpaca-traders.money-group :as money])
+  )
+
+(defn item-to-option [item prefix]
+  "Create an HTML Option. If value is nil, option is disabled. 
+  Defaults to disabled options."
+  (let [{value :id 
+         label :name} item]
+    [:option {:value value
+              :key (str prefix value)
+              } label]
+    )
+  )
+
+(defn item-select [] 
+  (let [items (subscribe [:items])
+        options  (map #(item-to-option % " item") @items)]
+    [:select {:key "item-select"
+              :on-change #(dispatch [:select-item (.-target.value %)])
+              :value @(subscribe [:item])} options])
+  )
+
+(defn server-select [] 
+  (let [servers (subscribe [:servers])
+        options  (map #(item-to-option % " server") @servers)]
+    [:select {:on-change #(dispatch [:select-server (.-target.value %)])
+              :value @(subscribe [:server])} options])
+  )
+
+(defn listings-table [listings]
+  [:div] 
+  )
+
+(defn create [] 
+  [:div.default-body.search
+   (item-select)
+   (-> @(subscribe [:item]))
+   (server-select)
+   (-> @(subscribe [:server]))
+   [:button.btn.btn-default 
+      {:type "button"
+       :on-click #(dispatch [:search-for-listings 1 2])}
+     "Search"]
+   (str @(subscribe [:listings]))
+   ]
+  )
