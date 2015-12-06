0f2c7729aa8547582906c67341fc71decf950292
palme1337 <chris.palmer.1337@icloud.com>
2015-12-05 16:57:08 -0800

    Placeholder for querying listings

--- src/clj/alpaca_traders/listings.clj
+++ src/clj/alpaca_traders/listings.clj
@@ -3,12 +3,33 @@
     [alpaca-traders.rethink-driver :as driver]
     [rethinkdb.query :as r]))
 
+;TODO: move to map probably.
+(defonce ALL "ALL")
+(defonce SERVER "server")
+(defonce ITEM "item")
+(defonce SERVER-ITEM "server-item")
+
+; (defn wildcard? [value] (= value "*")
+;   )
+
+; (defn query-type [item server] 
+;   (if (every? wildcard? [item server])
+;     "ALL-LISTINGS"
+;     )
+;   )
+
 (defn query [query-params] 
   (let [{:keys [item server]} query-params]
     (print query-params)(print query-params)(print query-params)(print query-params)
      (sorted-map))
-     
-  (with-open [conn (driver/get-conn)] 
-    (-> (r/db driver/DB_NAME)
-        (r/table "listings")
-        (r/run conn))))
+  
+  ; (if (every? wildcard? [item server])
+    
+  ;   )
+    
+  ; (with-open [conn (driver/get-conn)] 
+  ;   (-> (r/db driver/DB_NAME)
+  ;       (r/table "listings")
+  ;       (r/run conn)))
+  
+  )
