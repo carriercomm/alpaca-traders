9587d824eac145fdb3e84d663930434920d400ce
palme1337 <chris.palmer.1337@icloud.com>
2015-12-05 16:42:51 -0800

    Add secondary indexes on the tablez

--- src/clj/alpaca_traders/rethink_driver.clj
+++ src/clj/alpaca_traders/rethink_driver.clj
@@ -11,16 +11,16 @@
   (r/connect :host DB_ADDR :port DB_PORT :db DB_NAME))
 
 (defn create-db [conn db] 
- "create db if exists."
+  "create db if exists."
   (println "Creating DB: " db)
   (let [db-exists? (some #{db} (r/run (r/db-list) conn))]
     (if db-exists?
-      (println "No need to create DB. Exists")
+      (println "\tNo need to create DB. Exists")
       (r/run (r/db-create db) conn))
     ))
 
 (defn table-exists? [conn db table-name] 
-   (some #{table-name} (r/run (r/table-list) conn)))
+  (some #{table-name} (r/run (r/table-list) conn)))
 
 (defn drop-table [conn db table-name] 
   "create table if eists"
@@ -29,18 +29,49 @@
     (-> (r/db db)
         (r/table-drop table-name)
         (r/run conn))
-    (println "No need to drop table. Doesnt exist.")
+    (println "\tNo need to drop table. Does not exist.")
     ))
 
 (defn create-table [conn db table-name] 
   "create table if eists"
   (println "Creating table: " db ":" table-name)
   (if (table-exists? conn db table-name)
-    (println "No need to create table. Exists.")
+    (println "\tNo need to create table. Exists.")
     (-> (r/db db)
         (r/table-create table-name)
         (r/run conn))
     ))
+
+(defn index-exists? [conn db table-name index-name] 
+  (some #{index-name} (-> (r/db db)
+                          (r/table table-name)
+                          (r/index-list)
+                          (r/run conn))))
+
+(defn create-index-server-item [conn db]
+  "Creates a compound index on server and item in the table 'listings'"
+  (let [table-name "listings"
+        index-name "server-item"]
+    (println "Creating listings index: " db ":" index-name)
+    (if-not (index-exists? conn db table-name index-name)
+      (-> (r/db db)
+          (r/table table-name)
+          (r/index-create index-name 
+                          (r/fn [row]
+                                [(r/get-field row :server-id)
+                                 (r/get-field row :item-id)]))
+          (r/run conn))
+      (println "\tDidn't create index")
+      )))
+
+(defn create-simple-index [conn db table-name index-kw] 
+  (if-not (index-exists? conn db table-name index-kw)
+      (-> (r/db db)
+          (r/table table-name)
+          (r/index-create index-kw)
+          (r/run conn))
+      ;TODO: wait for index creation?
+      ))
 
 (defn load-documents [conn db table-name] 
   (println "Loading documents for table " table-name)
@@ -68,12 +99,15 @@
       (make-table "items")
       (make-table "servers"))
     
+    (create-index-server-item conn DB_NAME)
+    (let [make-index #(create-simple-index conn DB_NAME "listings" %)]
+      (make-index :server)
+      (make-index :item))
+    
     ;Init the DB with the items listed in the json file. 
     (let [load-docs #(load-documents conn DB_NAME %)]
       (load-docs "items")
-      (load-docs "servers")
-      )
+      (load-docs "servers"))
     ))
-    
+
 ;(setup-db)
-  
